ad3f7b005bb9b2d17d788565a263af4178e71a54
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 22:22:36 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="landing-page">
+            <h1>Paradise Nursery</h1>
+            <p>Welcome to our plant shop!</p>
+            <button className="get-started-btn">Get Started</button>
+        </div>
+    );
+};
+
+export default App;
