e079fac0c4f16c3f8abf71593e5925d06c2ad58b
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 22:22:49 +0700

    Create CartSlice.jsx

---
+++ CartSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+    },
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload.name);
+        },
+        updateQuantity: (state, action) => {
+            const item = state.items.find(item => item.name === action.payload.name);
+            if (item) {
+                item.quantity = action.payload.quantity;
+            }
+        },
+    },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export default CartSlice.reducer;
