0251643c3e6195f98b505a65a2a1fd0a389b9bc1
Nguyen Huu Thanh <thanhchatho12345678@gmail.com>
2026-07-19 22:22:08 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <h1>About Paradise Nursery</h1>
+            <p>Welcome to Paradise Nursery, your number one source for all houseplants. We are dedicated to providing you the best company details.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
